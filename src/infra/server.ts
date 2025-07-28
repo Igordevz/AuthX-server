c27900e05378d7cf2871ce3bc95665edc38e75f3
Igordevz <programadorigor.dev@gmail.com>
2025-07-28 12:15:27 -0300

    update server

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -8,8 +8,9 @@ const app = fastify({
 
 app.register(fastifyCors, {
   origin: "*",
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "jwt", "*"],
 });
 
 app.register(CreateRouter);
